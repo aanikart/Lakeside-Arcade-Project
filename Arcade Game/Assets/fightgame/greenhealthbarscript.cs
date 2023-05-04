7715cfcf8129af6779aee981a8218077c6bd5c0f
Teiseat Hailemariam <TeiseatH23@lakesideschool.org>
2023-05-04 12:57:19 -0700

    hitboxes + hurtboxes + pushboxes + health bars for both fighters

---
+++ Arcade Game/Assets/fightgame/greenhealthbarscript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class greenhealthbarscript : MonoBehaviour
+{
+    public greenfightscript greenfightscript;
+    public Image fillImage;
+    private Slider slider;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.maxValue = greenfightscript.maxhealth;
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        else if (slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+        float fill = greenfightscript.health;
+        if (fill <= slider.maxValue / 3)
+        {
+            fillImage.color = Color.red;
+        }
+        else if (fill > slider.maxValue / 3)
+        {
+            fillImage.color = new Color(0, 130, 30, 255);
+        }
+        slider.value = fill;
+    }
+}
